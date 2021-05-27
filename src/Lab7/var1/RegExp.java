be5ff91b96ca625ba0618fa0d94180090ff64351
artemzhidkov <artem_zhidkov@mail.ru>
2021-05-28 01:07:28 +0300

    lab7

--- src/Lab7/var1/RegExp.java
+++ src/Lab7/var1/RegExp.java
@@ -5,9 +5,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
 
 public class RegExp {
-    static Path FILE = Paths.get("src/source/Lab7_var1.txt");
+    private static final Path FILE = Paths.get("src/source/Lab7_var1.txt");
+    private static final String FORMAT = "%3s";
+
     private int k = 3;
     private char symbol = '?';
 
@@ -24,6 +27,32 @@ public class RegExp {
         List<String> list = OpenFile();
         for (String line:list) {
             System.out.println(line.replaceAll(String.format("(\\b\\p{L}{%d})\\p{L}", k),"$1" + symbol));
+        }
+    }
+
+    private static void printSymbols(final String line) {
+        for (char symbol : line.toCharArray()) {
+            System.out.printf(FORMAT, symbol);
+        }
+        System.out.println();
+    }
+
+    private static void printSymbolNumbers(final String lines) {
+        for (char symbol : lines.toLowerCase().toCharArray()) {
+            if (Character.isLetter(symbol)) {
+                System.out.printf(FORMAT, (int)symbol - 1071);
+            } else {
+                System.out.printf(FORMAT, " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public void PrintCharNumber() {
+        List<String> lines = OpenFile();
+        for (final String line : lines) {
+            printSymbols(line);
+            printSymbolNumbers(line);
         }
     }
 }
